6c90b161ea25e0b0495a05ada88a3cafdcd7e695
IraLapina <ksushakrolikova04052009@gmail.com>
2026-03-13 16:47:07 +0300

    Создала программу "генератор паролей".

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+alphabet = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+length = int(input('Введите длину пароля: '))
+password = ''
+for i in range(length):
+    password += random.choice(alphabet)
+
+print('Ваш пароль: ',password)
